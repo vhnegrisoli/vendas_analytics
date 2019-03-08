0cc018bd89317b24f2a94c2faa8ac7be398c31d0
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-03-07 23:34:46 -0300

    Adiciona relatórios dashboard

--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/controller/DashboardController.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/controller/DashboardController.java
@@ -1,6 +1,8 @@
 package com.br.unifil.vendas_analytics.vendas_analytics.controller;
 
+import com.br.unifil.vendas_analytics.vendas_analytics.model.model_relatorios_dashboard.vendas_por_cliente;
 import com.br.unifil.vendas_analytics.vendas_analytics.model.model_relatorios_dashboard.vendas_por_periodo;
+import com.br.unifil.vendas_analytics.vendas_analytics.repository.repository_relatorios_dashboard.Vendas_Por_ClienteRepository;
 import com.br.unifil.vendas_analytics.vendas_analytics.repository.repository_relatorios_dashboard.Vendas_Por_PeriodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,9 +20,17 @@ public class DashboardController {
     @Autowired
     Vendas_Por_PeriodoRepository vendas_por_periodoRepository;
 
+    @Autowired
+    Vendas_Por_ClienteRepository vendas_por_clienteRepository;
+
     @GetMapping("/vendas-por-periodo")
     public List<vendas_por_periodo> getAllVendasPorPeriodo() {
         return vendas_por_periodoRepository.findAll();
+    }
+
+    @GetMapping("/card1/vendas-por-cliente")
+    public List<vendas_por_cliente> getAllVendasPorCliente() {
+        return vendas_por_clienteRepository.findAll();
     }
 
 }
--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/model/model_relatorios_dashboard/vendas_por_cliente.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/model/model_relatorios_dashboard/vendas_por_cliente.java
@@ -6,25 +6,19 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "VENDAS_POR_PERIODO")
+@Table(name = "VENDAS_POR_CLIENTE")
 @Data
 @NoArgsConstructor
-public class vendas_por_periodo {
+public class vendas_por_cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "quantidade_de_vendas")
-    private int quantidade;
+    @Column(name = "Clientes")
+    private int clientes;
 
-    @Column(name = "lucro_total")
-    private double lucro;
-
-    @Column(name = "lucro_medio_mensal")
-    private double media;
-
-    @Column(name = "meses")
+    @Column(name = "Meses")
     private String meses;
 
 }
