a8cafe3b83a6ec47b50dc972b52c13c56751d1b8
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2019-03-13 09:35:45 -0300

    Adiciona banner e remove autenticação temporariamente

--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/CustomUserDetailsService.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/CustomUserDetailsService.java
@@ -1,41 +1,41 @@
-package com.br.unifil.vendas_analytics.vendas_analytics.config;
-
-import com.br.unifil.vendas_analytics.vendas_analytics.model.Usuario;
-import com.br.unifil.vendas_analytics.vendas_analytics.repository.UsuarioRepository;
-import com.br.unifil.vendas_analytics.vendas_analytics.validation.ValidacaoException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
-import static com.br.unifil.vendas_analytics.vendas_analytics.enums.UsuarioSituacao.ATIVO;
-
-@Service("customUserDetailsService")
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Usuario usuario = usuarioRepository
-                .findByEmailAndSituacao(email, ATIVO)
-            .orElseThrow(() -> new ValidacaoException("Usuário não encontrado"));
-        List<GrantedAuthority> authorityListAdmin = AuthorityUtils
-                .createAuthorityList("ROLE_" + usuario.getPermissoesUsuario().getPermissao().name());
-        return new User(
-                usuario.getEmail(),
-                usuario.getSenha(),
-                authorityListAdmin);
-    }
-}
+//package com.br.unifil.vendas_analytics.vendas_analytics.config;
+//
+//import com.br.unifil.vendas_analytics.vendas_analytics.model.Usuario;
+//import com.br.unifil.vendas_analytics.vendas_analytics.repository.UsuarioRepository;
+//import com.br.unifil.vendas_analytics.vendas_analytics.validation.ValidacaoException;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.context.request.RequestContextHolder;
+//import org.springframework.web.context.request.ServletRequestAttributes;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import java.util.List;
+//
+//import static com.br.unifil.vendas_analytics.vendas_analytics.enums.UsuarioSituacao.ATIVO;
+//
+//@Service("customUserDetailsService")
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private UsuarioRepository usuarioRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        Usuario usuario = usuarioRepository
+//                .findByEmailAndSituacao(email, ATIVO)
+//            .orElseThrow(() -> new ValidacaoException("Usuário não encontrado"));
+//        List<GrantedAuthority> authorityListAdmin = AuthorityUtils
+//                .createAuthorityList("ROLE_" + usuario.getPermissoesUsuario().getPermissao().name());
+//        return new User(
+//                usuario.getEmail(),
+//                usuario.getSenha(),
+//                authorityListAdmin);
+//    }
+//}
--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/SecurityConfig.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/SecurityConfig.java
@@ -1,43 +1,43 @@
-package com.br.unifil.vendas_analytics.vendas_analytics.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailService;
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(customUserDetailService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-}
+//package com.br.unifil.vendas_analytics.vendas_analytics.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private CustomUserDetailsService customUserDetailService;
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .userDetailsService(customUserDetailService)
+//                .passwordEncoder(new BCryptPasswordEncoder());
+//    }
+//
+//}
