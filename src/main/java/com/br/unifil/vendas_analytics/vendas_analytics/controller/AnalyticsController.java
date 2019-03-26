a8b9ea94702b6d4ed1395fa5512d4e6c70377945
Laboratório Informática <labinfo@unifil.br>
2019-03-25 22:10:36 -0300

    Adiciona relatório vendas_por_categorias

--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/controller/AnalyticsController.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/controller/AnalyticsController.java
@@ -1,4 +1,26 @@
 package com.br.unifil.vendas_analytics.vendas_analytics.controller;
 
+import com.br.unifil.vendas_analytics.vendas_analytics.model.model_relatorios_dashboard.vendas_por_categoria;
+import com.br.unifil.vendas_analytics.vendas_analytics.repository.Vendas_Por_CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/analytics")
 public class AnalyticsController {
+
+    @Autowired
+    private Vendas_Por_CategoriaRepository vendas_por_categoriaRepository;
+
+    @GetMapping("/vendas-por-categoria")
+    public List<vendas_por_categoria> getAllVendasPorCategoria() {
+        return vendas_por_categoriaRepository.findAll();
+    }
+
 }
