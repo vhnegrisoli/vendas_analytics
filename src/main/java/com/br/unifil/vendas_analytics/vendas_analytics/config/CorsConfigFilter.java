bb48c9cc659860db25d7778181f14123002a4ccb
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2019-07-23 12:33:50 -0300

    Altera cors

--- src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/CorsConfigFilter.java
+++ src/main/java/com/br/unifil/vendas_analytics/vendas_analytics/config/CorsConfigFilter.java
@@ -13,7 +13,7 @@ public class CorsConfigFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
